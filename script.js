0ca4a57358fc26b1f50692f729c3e1928e31f812
BlueQuantum <blue_quantum@outlook.com>
2020-08-28 10:16:08 +0800

    New Art

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
 var flag = 0;
+
+var offsX = document.documentElement.clientWidth / 2;
+var offsY = document.documentElement.clientHeight / 2;
+var list = document.getElementsByClassName("card");
+var height = document.documentElement.clientHeight;
+
+document.onmousemove = function (event) {
+  // document.getElementById("posX").innerHTML = X;
+  // document.getElementById("posY").innerHTML = Y;
+  let X = event.pageX;
+  let Y = event.pageY;
+  let sway = document.getElementById("sway-pic");
+  let star = document.getElementById("starback");
+  let back = document.getElementById("back");
+  sway.style.transform = "translate(" + (X - offsX) / 15 + "px" + "," + (Y - offsY) / 15 + "px)";
+  star.style.transform = "translate(" + (X - offsX) / 30 + "px" + "," + (Y - offsY) / 30 + "px)";
+  back.style.transform = "translate(" + (X - offsX) / 60 + "px" + "," + (Y - offsY) / 60 + "px)";
+}
+
 window.onscroll = function () {
-  var height = document.documentElement.clientHeight;
-  // document.getElementById("A").innerHTML = height;
-  // document.getElementById("B").innerHTML = document.documentElement.scrollTop;
+  // document.getElementById("scroll").innerHTML = document.documentElement.scrollTop;
+  if (document.documentElement.scrollTop < 0.01) {
+    document.getElementById("top-panel").style.boxShadow = "0 0 0 black";
+  } else {
+    document.getElementById("top-panel").style.boxShadow = "0 0 8px rgba(0, 0, 0, 0.4)";
+  }
+
   if (flag == 0 && document.documentElement.scrollTop > height * 0.75) {
-    var list = document.getElementsByClassName("card");
-    for (let i = 0; i < list.length; ++i) {
-      const element = list[i];
+    for (let i = 0; i < this.list.length; ++i) {
+      let element = this.list[i];
       element.style.animation = "cutin 1s ease";
       element.style.opacity = "1";
     }
+    document.getElementById("main-container").style.pointerEvents = "auto";
     flag = 1;
   }
   if (flag == 1 && document.documentElement.scrollTop < height * 0.6) {
-    var list = document.getElementsByClassName("card");
-    for (let i = 0; i < list.length; ++i) {
-      const element = list[i];
+    for (let i = 0; i < this.list.length; ++i) {
+      let element = this.list[i];
       element.style.animation = "cutout 1s ease";
       element.style.opacity = "0";
     }
+    document.getElementById("main-container").style.pointerEvents = "none";
     flag = 0;
   }
 }
