ef2dbe210ffda25e88cf32750a3cab708e093ddb
BlueQuantum <blue_quantum@outlook.com>
2020-10-25 16:35:33 +0800

    新增Goline-text特效，评论排版优化，Bug fix

--- script.js
+++ script.js
@@ -1,47 +1,36 @@
-var flag = 0;
+var flag = false;
 
 var offsX = document.documentElement.clientWidth / 2;
 var offsY = document.documentElement.clientHeight / 2;
-var list = document.getElementsByClassName("card");
+var mainContainer = document.getElementById("main-container");
+var sway = document.getElementById("sway");
+var topPanel = document.getElementById("top-panel");
 var height = document.documentElement.clientHeight;
 
-document.onmousemove = function (event) {
+window.onmousemove = function (event) {
   // document.getElementById("posX").innerHTML = X;
   // document.getElementById("posY").innerHTML = Y;
-  let X = event.pageX;
-  let Y = event.pageY;
-  let sway = document.getElementById("sway-pic");
-  let star = document.getElementById("starback");
-  let back = document.getElementById("back");
-  sway.style.transform = "translate(" + (X - offsX) / 15 + "px" + "," + (Y - offsY) / 15 + "px)";
-  star.style.transform = "translate(" + (offsX - X) / 30 + "px" + "," + (offsY - Y) / 30 + "px)";
-  back.style.transform = "translate(" + (offsX - X) / 60 + "px" + "," + (offsY - Y) / 60 + "px)";
+  sway.style.transform = "translate(" + (event.pageX - offsX) * 0.1 + "px" + "," + (event.pageY - offsY) * 0.1 + "px)";
 }
 
 window.onscroll = function () {
   // document.getElementById("scroll").innerHTML = document.documentElement.scrollTop;
   if (document.documentElement.scrollTop < 0.01) {
-    document.getElementById("top-panel").style.boxShadow = "0 0 0 black";
+    topPanel.style.boxShadow = "0 0 0 black";
   } else {
-    document.getElementById("top-panel").style.boxShadow = "0 0 8px rgba(0, 0, 0, 0.4)";
+    topPanel.style.boxShadow = "0 0 8px rgba(0, 0, 0, 0.4)";
   }
 
-  if (flag == 0 && document.documentElement.scrollTop > height * 0.75) {
-    for (let i = 0; i < this.list.length; ++i) {
-      let element = this.list[i];
-      element.style.animation = "cutin 1s ease";
-      element.style.opacity = "1";
-    }
-    document.getElementById("main-container").style.pointerEvents = "auto";
+  if (flag == false && document.documentElement.scrollTop > height * 0.75) {
+    mainContainer.style.animation = "cutin 1s ease";
+    mainContainer.style.opacity = "1";
+    mainContainer.style.pointerEvents = "auto";
     flag = 1;
   }
-  if (flag == 1 && document.documentElement.scrollTop < height * 0.6) {
-    for (let i = 0; i < this.list.length; ++i) {
-      let element = this.list[i];
-      element.style.animation = "cutout 1s ease";
-      element.style.opacity = "0";
-    }
-    document.getElementById("main-container").style.pointerEvents = "none";
+  if (flag == true && document.documentElement.scrollTop < height * 0.6) {
+    mainContainer.style.animation = "cutout 1s ease";
+    mainContainer.style.opacity = "0";
+    mainContainer.style.pointerEvents = "none";
     flag = 0;
   }
 }
@@ -54,8 +43,7 @@ var gitalk = new Gitalk({
   repo: 'MyBlogComments',//仓库名称
   owner: 'Bluequarks',//仓库拥有者
   admin: ['Bluequarks'],
-  // id: decodeURI(window.location.pathname),
-  id: 'test',
+  id: 'commentsTest',
   distractionFreeMode: false  // Facebook-like distraction free mode
 });
 
