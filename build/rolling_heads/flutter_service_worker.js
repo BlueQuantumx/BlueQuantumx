a18a791ae5fc0016802d0702ee513a0ec701bdad
BlueQuantum <blue_quantum@outlook.com>
2022-02-04 17:29:51 +0800

    A little more adjustments

--- build/rolling_heads/flutter_service_worker.js
+++ build/rolling_heads/flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
 "canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
 "canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
-"main.dart.js": "55d2e7fd2f0ebe6b9a4f3ea3e0b7d18d",
+"main.dart.js": "9e4677906a00519cefd555a5638c53c3",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -19,8 +19,8 @@ const RESOURCES = {
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"index.html": "4291bb0ebce0286eb8e21874f5102977",
-"/": "4291bb0ebce0286eb8e21874f5102977"
+"index.html": "1f7958beea30e95fb79b12e6e9109551",
+"/": "1f7958beea30e95fb79b12e6e9109551"
 };
 
 // The application shell files that are downloaded before a service worker can
